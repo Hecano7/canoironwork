1601ee5be3f71d75aff5bb5ec5bfe7e8607348e4
Hecan7 <7hecano@gmail.com>
2020-09-11 14:18:19 -0700

    Solution

--- Public/script.js
+++ Public/script.js
@@ -1,28 +1,29 @@
   let photo;
   let inpFile = document.getElementById("inpFile");
   let previewContainer = document.getElementById("imagePreview");
-  let previewImage = previewContainer.querySelector(".image-preview__image");
-  let previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
-  
-  inpFile.addEventListener("change", function(){
-    let file = this.files[0];
-    if(file){
-      let reader = new FileReader();
-      previewDefaultText.style.display = "none";
-      previewImage.style.display = "block";
 
-      reader.addEventListener("load", function(){
-        console.log(this.result);
-        photo = this.result;
-        previewImage.setAttribute("src", this.result)
-      });
-      reader.readAsDataURL(file)
-    }else{
-      previewDefaultText.style.display = null;
-      previewImage.style.display = null;
-      previewImage.setAttribute("src","");
-    }
-  })
+  // let previewImage = previewContainer.querySelector(".image-preview__image");
+  // let previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+  
+  // inpFile.addEventListener("change", function(){
+  //   let file = this.files[0];
+  //   if(file){
+  //     let reader = new FileReader();
+  //     previewDefaultText.style.display = "none";
+  //     previewImage.style.display = "block";
+
+  //     reader.addEventListener("load", function(){
+  //       console.log(this.result);
+  //       photo = this.result;
+  //       previewImage.setAttribute("src", this.result)
+  //     });
+  //     reader.readAsDataURL(file)
+  //   }else{
+  //     previewDefaultText.style.display = null;
+  //     previewImage.style.display = null;
+  //     previewImage.setAttribute("src","");
+  //   }
+  // })
 
   function sendEmail() {
     var name = document.getElementById("name").value;
@@ -33,3 +34,8 @@
     }
   };
   
+  function imageClick(element){
+    document.getElementById("enlarge").src = element.src;
+    window.location.href = "#dog";
+    // console.log(element.children[0].src);
+  }
