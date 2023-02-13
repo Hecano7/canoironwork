a3486ae32ee168cee795bb2099c3ba78294393b9
Hecan7 <7hecano@gmail.com>
2023-02-12 23:52:22 -0800

    vercel

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'Public/index.html'));
 });
 
 app.get('/gallery', function(req, res) {
